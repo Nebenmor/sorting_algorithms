8b460c406d758063c1ca8654dba2423101e564c8
nebenmor <nebenmor.anthony@gmail.com>
2024-02-20 06:48:36 -0800

    2-main.c            2-O                 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,6 +6,7 @@
  * @b: Is the second integer to swap.
  */
 
+void swap_ints(int *a, int *b);
 void swap_ints(int *a, int *b)
 {
 	int tmp;
@@ -23,6 +24,8 @@ void swap_ints(int *a, int *b)
  *
  * Description: This prints the array after each swap.
  */
+
+void selection_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size)
 {
 	int *min;
